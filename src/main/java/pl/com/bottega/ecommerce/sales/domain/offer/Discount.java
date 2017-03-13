b9ba97ab39afcd3cb4699ed79c1c11b1e16caed4
aleksandradura <aleksandradura_95@onet.pl>
2017-03-13 16:33:25 +0100

    add class 'Discount'

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Discount.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Discount.java
@@ -5,10 +5,54 @@
  */
 package pl.com.bottega.ecommerce.sales.domain.offer;
 
+import java.math.BigDecimal;
+
 /**
  *
  * @author 194938
  */
 public class Discount {
+    private String discountCause;
+    private BigDecimal discount;
+
+    public BigDecimal getDiscount() {
+        return discount;
+    }
+
+    public String getDiscountCause() {
+        return discountCause;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                + ((discount == null) ? 0 : discount.hashCode());
+        result = prime * result + ((discountCause == null) ? 0 : discountCause.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Discount disc = (Discount) obj;
+        if (discount == null) {
+            if (disc.discount != null)
+                return false;
+        } else if (!discount.equals(disc.discount))
+            return false;
+        if (discountCause == null) {
+            if (disc.discountCause != null)
+                return false;
+        } else if (!discountCause.equals(disc.discountCause))
+            return false;
+        return true;
+    }
     
 }
